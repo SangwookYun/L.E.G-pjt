3b776c8d66bc6d00476ffda38a929615b043d27f
jasonwei0224 <45651276+jasonwei0224@users.noreply.github.com>
2021-01-29 14:40:03 -0800

    finish login

--- src/index.js
+++ src/index.js
@@ -3,12 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase/app";
+import 'firebase/auth';
+import 'firebase/analytics'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+var firebaseConfig = {
+    apiKey: "AIzaSyDtQ1w3yX-lFf1zuh1zm41gFQKnZ0_s7nU",
+    authDomain: "legbackend.firebaseapp.com",
+    databaseURL: "https://legbackend-default-rtdb.firebaseio.com",
+    projectId: "legbackend",
+    storageBucket: "legbackend.appspot.com",
+    messagingSenderId: "46408878340",
+    appId: "1:46408878340:web:00a2a59589003a5db1a851",
+    measurementId: "G-V2HB3FQMMT"
+};
+
+firebase.initializeApp(firebaseConfig)
+firebase.analytics();
+
+ReactDOM.render( <
+    React.StrictMode >
+    <
+    App / >
+    <
+    /React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
